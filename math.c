06de444b0c1462190c46e85f385f8a7b9f7e37ce
Yousuke Nakao <yoosukenakao@thewaggle.co.jp>
2017-07-01 17:43:56 +0900

    Lesson 2追加中

---
+++ math.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    printf("足し算＋：１ + ２ = %d\n", 1+2);
+    printf("引き算ー：１ - ２ = %d\n", 1-2);
+    printf("掛け算×：２ * ３ = %d\n", 2*3);
+    printf("割り算＋：１ / ２ = %d\n", 1+2);
+    printf("剰余算（割り算の余り）：１ ％ ２ = %d\n", 1+2);
+    return 0;
+}
